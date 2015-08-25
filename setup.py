3cf50aca095748d208a3fea29a55bb348eb0b609
vforgione <v.forgione@gmail.com>
2015-08-25 17:27:11 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pageview_digest",
-    version="0.0.2",
+    version="0.0.3",
 
     packages=["pageview_digest", ],
     install_requires=["gevent==1.0.2", "psycopg2==2.6.1", "pylibmc==1.5.0", "pytz==2015.4", ],
