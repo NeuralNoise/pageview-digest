4bdfd82daef1633ee6e1f288597f1674180b8234
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-12 17:01:14 -0500

    successful response

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,9 +1,8 @@
 import requests
 
-import pg8000
+import psycopg2
 import pytest
 import testing.postgresql
-from contextlib import closing
 from pytest_localserver.http import WSGIServer
 
 from pageview_digest.wsgi import application, db_user, db_passwd, db_dbname
@@ -14,17 +13,41 @@ class TestWSGIApplication:
     @pytest.fixture()
     def setup_postgresql(self):
         self.postgresql = testing.postgresql.Postgresql(port=5432)
-        with closing(pg8000.connect(**self.postgresql.dsn())) as conn:
-            conn.autocommit = True
-            with closing(conn.cursor()) as cursor:
-                # Create configured user if doesn't exist
-                cursor.execute("SELECT COUNT(*) FROM pg_roles WHERE rolname='{}'".format(db_user))
-                if cursor.fetchone()[0] <= 0:
-                    cursor.execute("CREATE USER {0} WITH PASSWORD '{1}'".format(db_user, db_passwd))
-                # Create configured database
-                cursor.execute("SELECT COUNT(*) FROM pg_database WHERE datname='{}'".format(db_dbname))
-                if cursor.fetchone()[0] <= 0:
-                    cursor.execute("CREATE DATABASE {}".format(db_dbname))
+        conn = psycopg2.connect(**self.postgresql.dsn())
+        conn.autocommit = True
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        # Create configured user if doesn't exist
+        cursor.execute("SELECT COUNT(*) FROM pg_roles WHERE rolname='{}'".format(db_user))
+        if cursor.fetchone()[0] <= 0:
+            cursor.execute("CREATE USER {0} WITH PASSWORD '{1}'".format(
+                db_user, db_passwd
+            ))
+
+        # Create configured database
+        cursor.execute(
+            "SELECT COUNT(*) FROM pg_database WHERE datname='{}'".format(db_dbname)
+        )
+        if cursor.fetchone()[0] <= 0:
+            cursor.execute("CREATE DATABASE {}".format(db_dbname))
+
+        conn.close()
+        cursor.close()
+        # Connect to the newly created database
+        ingest_dsn = self.postgresql.dsn().copy()
+        ingest_dsn.update({
+            'database': db_dbname,
+            'user': db_user,
+            'password': db_passwd
+        })
+
+        conn = psycopg2.connect(**ingest_dsn)
+        conn.autocommit = True
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        # Load all of our beautiful site tables
+        cursor.execute(open('./ansible/files/tables.sql', 'r').read())
+        cursor.close()
 
     @pytest.fixture(autouse=True)
     def testserver(self, request):
@@ -37,7 +60,7 @@ class TestWSGIApplication:
 
     def test_response_success(self, testserver):
         """assert app returns a 200"""
-        resp = requests.get(testserver.trending_url, params={'site': 'onion'})
+        resp = requests.get(testserver.trending_url, params={'site': 'theonion'})
         assert resp.status_code == 200
 
     def test_response_not_found(self, testserver):
