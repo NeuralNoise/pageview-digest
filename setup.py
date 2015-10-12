7120f758cddcfef7f0e964bbb6b39f74f00119cb
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-12 17:46:34 -0500

    update setup.py dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools.command.test import test as TestCommand # noqa
+
 from setuptools import setup
+
+dev_requires = [
+    "flake8>=2.0,<2.1",
+    "pytest==2.6.4",
+    "pytest-dbfixtures==0.12.0",
+    "pytest-cov==1.8.1",
+    "pytest-localserver==0.3.4",
+    "testing.postgresql==1.2.1",
+    "coveralls==0.5",
+    "mkdocs==0.12.2",
+    "requests==2.8.0"
+]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name="pageview_digest",
@@ -13,4 +45,9 @@ setup(
     license="MIT",
     keywords=["uwsgi gevent postgres pageview"],
     url="https://github.com/theonion/pageview-digest",
+    tests_require=dev_requires,
+    extras_require={
+        'dev': dev_requires,
+    },
+    cmdclass={'test': PyTest}
 )
