1f6b4cac7b762e5db407918250d41e2d03ec6800
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-16 13:55:06 -0500

    bump the version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 
 setup(
     name="pageview_digest",
-    version="0.0.5",
+    version="0.0.6",
 
     packages=["pageview_digest", ],
     install_requires=["gevent==1.0.2", "psycopg2==2.6.1", "pylibmc==1.5.0", "python-dateutil==2.4.2", ],
